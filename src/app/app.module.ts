43277631c8319af2b3dc50cf092245d5d268b6d6
atiwat_t <atiiwwat@gmail.com>
2022-07-19 21:37:30 +0700

    initializeApp firebaseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BillsComponent } from './pages/bills/bills.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
-const app = initializeApp(environment.firebaseConfig);
+// const app = initializeApp(environment.firebaseConfig);
+const app = initializeApp({
+  apiKey: "AIzaSyBjbzRq8GRGs6GCIvoaXKMbVm1Z3IO7Nzk",
+  authDomain: "lottery-management-aa429.firebaseapp.com",
+  databaseURL: "https://lottery-management-aa429-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lottery-management-aa429",
+  storageBucket: "lottery-management-aa429.appspot.com",
+  messagingSenderId: "971943506776",
+  appId: "1:971943506776:web:5b49f8e061d1036d636b2d",
+  measurementId: "G-ZWQPJ4W9HN"
+});
 const analytics = getAnalytics(app);
 
 @NgModule({
