ed51735f6550a9eb378a1b68dd579c10ab0781ec
atiwat_t <atiiwwat@gmail.com>
2022-07-19 21:27:43 +0700

    env production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjbzRq8GRGs6GCIvoaXKMbVm1Z3IO7Nzk",
+    authDomain: "lottery-management-aa429.firebaseapp.com",
+    databaseURL: "https://lottery-management-aa429-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "lottery-management-aa429",
+    storageBucket: "lottery-management-aa429.appspot.com",
+    messagingSenderId: "971943506776",
+    appId: "1:971943506776:web:5b49f8e061d1036d636b2d",
+    measurementId: "G-ZWQPJ4W9HN"
+  }
 };
